d0a849f4e4cae5c4fc317a53f9753c8b113b4d6b
ghostbuster91 <a@a.pl>
2015-04-16 14:56:21 +0200

    checking java8

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,7 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @RestController
 public class GreetingController {
@@ -14,6 +19,13 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        List<Integer> someList = new ArrayList<Integer>();
+        someList.add(2);
+        someList.add(2);
+        someList.add(2);
+
+        someList.stream().filter( i -> i > 20).collect(Collectors.toList());
+
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
