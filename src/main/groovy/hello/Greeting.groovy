1ab58c2d2d712bf167bba7353e6019662fa15387
ghostbuster91 <a@a.pl>
2015-04-17 11:20:51 +0200

    grooving the world!

---
+++ src/main/groovy/hello/Greeting.groovy
@@ -0,0 +1,13 @@
+package hello;
+
+class Greeting {
+
+    final long id;
+    final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+}
