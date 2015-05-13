466b61bab3e28453c0027404f7efd223412764ef
ghost <kas_kon5@wp.pl>
2015-05-13 19:59:46 +0200

    cache example

--- src/main/groovy/com/ghostbuster/springOnHeroku/controller/GreetingController.groovy
+++ src/main/groovy/com/ghostbuster/springOnHeroku/controller/GreetingController.groovy
@@ -1,7 +1,9 @@
 package com.ghostbuster.springOnHeroku.controller
 
 import com.ghostbuster.springOnHeroku.model.Greeting
+import com.ghostbuster.springOnHeroku.repository.GreetingRepository
 import groovy.transform.CompileStatic
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -10,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class GreetingController {
 
+    @Autowired
+    private GreetingRepository greetingRepository
+
+
     @RequestMapping('/greeting')
     public Greeting search(@RequestParam(value = "content", defaultValue = "World") String name) {
-        return new Greeting("Hello ${name}!!")
+        return greetingRepository.getGreeting()
     }
 }
---
+++ src/main/groovy/com/ghostbuster/springOnHeroku/repository/GreetingRepository.groovy
@@ -0,0 +1,27 @@
+package com.ghostbuster.springOnHeroku.repository
+
+import com.ghostbuster.springOnHeroku.model.Greeting
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Component
+
+@Component
+class GreetingRepository {
+
+    @Override
+    @Cacheable("someCache")
+    public Greeting getGreeting() {
+        simulateSlowService();
+        return new Greeting()
+    }
+
+    // Don't do this at home
+    private void simulateSlowService() {
+        try {
+            long time = 5000L;
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+}
